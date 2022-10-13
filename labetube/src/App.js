a6ec09690bc728799a091efc627d1e8b22216281
renanpmiranda <renanpmiranda5@gmail.com>
2022-10-13 15:51:13 -0300

    a032_exercicios

--- labetube/src/App.js
+++ labetube/src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import { CardVideo } from './componentes/CardVideo';
 
 function App() {
-  const titulo = "Título do vídeo"
-
-  function reproduzVideo() {
-    alert("O vídeo está sendo reproduzido")
-}
-
-  return (
+    return (
     <div>
       <div className="tela-inteira">
         <header>
@@ -30,41 +24,16 @@ function App() {
             </nav>
 
             <section className="painel-de-videos">
-                <div className="box-pagina-principal media1" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=1 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media2" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=2 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media3" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=3 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media4" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=4 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media5" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=5 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media6" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=6 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media7" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=7 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
-                <div className="box-pagina-principal media8" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=8 " alt="" />
-                    <h4>{titulo}</h4>
-                </div>
+                <CardVideo titulo={"Título do vídeo 1"} imagem={"https://picsum.photos/400/400?a=1"}/> 
+                <CardVideo titulo={"Título do vídeo 2"} imagem={"https://picsum.photos/400/400?a=2"}/> 
+                <CardVideo titulo={"Título do vídeo 3"} imagem={"https://picsum.photos/400/400?a=3"}/> 
+                <CardVideo titulo={"Título do vídeo 4"} imagem={"https://picsum.photos/400/400?a=4"}/> 
+                <CardVideo titulo={"Título do vídeo 5"} imagem={"https://picsum.photos/400/400?a=5"}/> 
+                <CardVideo titulo={"Título do vídeo 6"} imagem={"https://picsum.photos/400/400?a=6"}/> 
+                <CardVideo titulo={"Título do vídeo 7"} imagem={"https://picsum.photos/400/400?a=7"}/> 
+                <CardVideo titulo={"Título do vídeo 8"} imagem={"https://picsum.photos/400/400?a=8"}/>                                              
             </section>
         </main>
-
         <footer>
             <h4>Oi! Eu moro no footer!</h4>
         </footer>
